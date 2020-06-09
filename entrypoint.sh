4f3187b9bae1953d38792105a3204fa77dccc2be
ogavva-t <ogavva.t@gmail.com>
2020-06-09 13:57:23 +0000

    add pjsip_trunk setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@
 sed -i "s/ENV_EXT_MEDIA_ADDRESS/$EXT_MEDIA_ADDRESS/" /etc/asterisk/pjsip.conf
 sed -i "s/ENV_EXT_SIGNALING_ADDRESS/$EXT_SIGNALING_ADDRESS/" /etc/asterisk/pjsip.conf
 
+sed -i "s/ENV_SIPTRUNK_IP/$SIPTRUNK_IP/" /etc/asterisk/pjsip_trunk.conf
+sed -i "s/ENV_SIPTRUNK_PORT/$SIPTRUNK_PORT/" /etc/asterisk/pjsip_trunk.conf
+
+
 # Start server.
 echo 'Starting Asterisk...'
 /usr/sbin/asterisk -vvvvvdddddc
