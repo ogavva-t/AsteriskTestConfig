b30c419906a7d2ab2e184731af4d091864b28528
ogavva-t <ogavva.t@gmail.com>
2020-06-10 23:23:06 +0900

    add EXTEN_PASSWORD environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Replace External Address Settings
 sed -i "s/ENV_EXT_MEDIA_ADDRESS/$EXT_MEDIA_ADDRESS/" /etc/asterisk/pjsip.conf
 sed -i "s/ENV_EXT_SIGNALING_ADDRESS/$EXT_SIGNALING_ADDRESS/" /etc/asterisk/pjsip.conf
+sed -i "s/ENV_EXTEN_PASSWORD/$EXTEN_PASSWORD/" /etc/asterisk/pjsip.conf
 
 sed -i "s/ENV_SIPTRUNK_IP/$SIPTRUNK_IP/" /etc/asterisk/pjsip_trunk.conf
 sed -i "s/ENV_SIPTRUNK_PORT/$SIPTRUNK_PORT/" /etc/asterisk/pjsip_trunk.conf
