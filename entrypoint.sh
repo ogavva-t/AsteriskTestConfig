5bc95c059a7c53a7eb58b427242009fdda499ee3
ogavva-t <ogavva-t@gmail.com>
2020-06-13 12:04:11 -0400

    add TRUNK_USERNAME, TRUNK_PASSWORD vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@ sed -i "s/ENV_EXT_MEDIA_ADDRESS/$EXT_MEDIA_ADDRESS/" /etc/asterisk/pjsip.conf
 sed -i "s/ENV_EXT_SIGNALING_ADDRESS/$EXT_SIGNALING_ADDRESS/" /etc/asterisk/pjsip.conf
 sed -i "s/ENV_EXTEN_PASSWORD/$EXTEN_PASSWORD/" /etc/asterisk/pjsip.conf
 
-sed -i "s/ENV_SIPTRUNK_IP/$SIPTRUNK_IP/" /etc/asterisk/pjsip_trunk.conf
-sed -i "s/ENV_SIPTRUNK_PORT/$SIPTRUNK_PORT/" /etc/asterisk/pjsip_trunk.conf
+sed -i "s/ENV_TRUNK_URL/$TRUNK_URL/" /etc/asterisk/pjsip_trunk.conf
+sed -i "s/ENV_TRUNK_PORT/$TRUNK_PORT/" /etc/asterisk/pjsip_trunk.conf
+sed -i "s/ENV_TRUNK_USERNAME/$TRUNK_USERNAME/" /etc/asterisk/pjsip_trunk.conf
+sed -i "s/ENV_TRUNK_PASSWORD/$TRUNK_PASSWORD/" /etc/asterisk/pjsip_trunk.conf
 
 
 # Start server.
