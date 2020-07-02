2d0d0697b8e63c69e82a51b566acec4ff80c64b2
Takayuki Ogawa <ogavva.t@gmail.com>
2020-07-02 12:33:59 +0900

    replace URL with Host

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ sed -i "s/ENV_EXT_MEDIA_ADDRESS/$EXT_MEDIA_ADDRESS/" /etc/asterisk/pjsip.conf
 sed -i "s/ENV_EXT_SIGNALING_ADDRESS/$EXT_SIGNALING_ADDRESS/" /etc/asterisk/pjsip.conf
 sed -i "s/ENV_EXTEN_PASSWORD/$EXTEN_PASSWORD/" /etc/asterisk/pjsip.conf
 
-sed -i "s/ENV_TRUNK_URL/$TRUNK_URL/" /etc/asterisk/pjsip_trunk.conf
+sed -i "s/ENV_TRUNK_HOST/$TRUNK_HOST/" /etc/asterisk/pjsip_trunk.conf
 sed -i "s/ENV_TRUNK_PORT/$TRUNK_PORT/" /etc/asterisk/pjsip_trunk.conf
-sed -i "s/ENV_TRUNK_URL/$TRUNK_URL/" /etc/asterisk/pjsip_trunk_noauth.conf
+sed -i "s/ENV_TRUNK_HOST/$TRUNK_HOST/" /etc/asterisk/pjsip_trunk_noauth.conf
 sed -i "s/ENV_TRUNK_PORT/$TRUNK_PORT/" /etc/asterisk/pjsip_trunk_noauth.conf
 
 sed -i "s/ENV_TRUNK_USERNAME/$TRUNK_USERNAME/" /etc/asterisk/pjsip_trunk.conf
